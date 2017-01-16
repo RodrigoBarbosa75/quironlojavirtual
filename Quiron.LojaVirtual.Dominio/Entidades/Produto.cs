de3fab784cf722c152d34406f0cde396aded6ee9
Rodrigo de Jesus Barbosa <rodrigobarbosa75@hotmail.com>
2017-01-16 16:52:05 -0200

    Commit Produto

---
+++ Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.Dominio.Entidades
+{
+    class Produto
+    {
+        public int ProdutoId { get; set; }
+        public String Nome { get; set; }
+        public String Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public String Categoria { get; set; }
+    }
+}
