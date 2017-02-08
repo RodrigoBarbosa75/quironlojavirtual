61bde4a72578c38c9f9ac46b412dd5a435f12ef1
Rodrigo de Jesus Barbosa <rodrigobarbosa75@hotmail.com>
2017-02-08 14:36:05 -0200

    Aula 13 Categoria

--- Quiron.LojaVirtual.Web/Controllers/VitrineController.cs
+++ Quiron.LojaVirtual.Web/Controllers/VitrineController.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
 using Quiron.LojaVirtual.Dominio.Repositorio;
 using Quiron.LojaVirtual.Web.Models;
 
@@ -13,24 +11,33 @@ namespace Quiron.LojaVirtual.Web.Controllers
         private ProdutosRepositorio _repositorio;
         public int ProdutosPorPagina = 8;
 
-        public ViewResult ListaProdutos(int pagina = 1)
+
+        public ViewResult ListaProdutos(string categoria, int pagina = 1)
         {
             _repositorio = new ProdutosRepositorio();
 
             ProdutosViewModel model = new ProdutosViewModel
             {
+
                 Produtos = _repositorio.Produtos
+                    .Where(p => p.Categoria == null || p.Categoria == categoria)
                     .OrderBy(p => p.Descricao)
                     .Skip((pagina - 1) * ProdutosPorPagina)
                     .Take(ProdutosPorPagina),
+
+
 
                 Paginacao = new Paginacao
                 {
                     PaginaAtual = pagina,
                     ItensPorPagina = ProdutosPorPagina,
                     ItensTotal = _repositorio.Produtos.Count()
-                }
+                },
+
+                CategoriaAtual = categoria
             };
+
+
             return View(model);
         }
     }
--- Quiron.LojaVirtual.Web/Models/ProdutosViewModel.cs
+++ Quiron.LojaVirtual.Web/Models/ProdutosViewModel.cs
@@ -1,16 +1,18 @@
-﻿using Quiron.LojaVirtual.Dominio.Entidades;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Quiron.LojaVirtual.Dominio.Entidades;
 
 namespace Quiron.LojaVirtual.Web.Models
 {
     public class ProdutosViewModel
     {
+
         public IEnumerable<Produto> Produtos { get; set; }
 
         public Paginacao Paginacao { get; set; }
 
+        public string CategoriaAtual { get; set; }
     }
 }
